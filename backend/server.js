e371fc2d6f4454d96f15e36184296db70f29dd05
Asif <asifsaheer7034@gmail.com>
2023-01-30 17:51:52 +0530

    upa

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
-const cors = require("cors");
+const cors = require("cors");  
 const fileUpload = require("express-fileupload");
 
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
